d9c758b92ff6dc08ca24f62c59df076aa4bf6095
Adam Chgour <adam.chgour@telecom-paris.fr>
2025-02-20 19:46:53 +0100

    Start of implementation of SVM.py and opti.py

--- src/opti.py
+++ src/opti.py
@@ -2,4 +2,25 @@ from abc import ABC, abstractmethod
 import pandas as pd
 import numpy as np
 
-#Implement Optimization methods in order to maximize the distance between the support vectors
+
+class Optimization(ABC):
+    @abstractmethod
+    def optimize(self, X, y,learning_step):
+        ''' Optimize the model to maximize the distance between the support vectors '''
+        pass
+
+class QuadraticProgrammingOptimization(Optimization):
+    def optimize(self, K, y, learning_rate=0.01, max_iter=1000):
+        ''' Solve the quadratic programming problem '''
+        n = K.shape[0]
+        phi = np.zeros(n)
+        
+        for _ in range(max_iter):
+            gradient = 2 * np.dot(K, phi) - np.ones(n)
+            phi -= learning_rate * gradient
+            
+            # Projection step to ensure constraints are satisfied
+            phi = np.maximum(phi, 0)  # Ensure phi >= 0
+            phi -= np.dot(y, phi) * y / np.dot(y, y)  # Ensure y.T @ phi = 0
+
+        return phi
--- src/svm.py
+++ src/svm.py
@@ -1,9 +1,43 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 import numpy as np
+import opti 
 
-def SVM(X_train,y_train): # TO IMPLEMENT
-    return None
+
+#Implement Optimization methods in order to maximize the distance between the support vectors
+#We first do a One vs One classification and then a One vs All classification
+#We will start by the case wehere the data is linearly separable and then implement the kernel trick for non-linearly separable data
+
+def SVM_OvO(X_train,y_train): # TO IMPLEMENT
+    def is_linearly_separable(X, y):
+        ''' Check if the given data is linearly separable '''
+        return np.all(np.dot(y * X, X.T) > 0)
     
-def SupportVector(X,y):
-    return None
+    def one_vs_one(X, y, i, j):
+        ''' Create a binary dataset for the given classes '''
+        mask = (y == i) | (y == j)
+        X = X[mask]
+        y = y[mask]
+        y = np.where(y == i, -1, 1)
+        return X, y
+    def train(X,y): # There might be an error to check
+        Optimizer = opti.QuadraticProgrammingOptimization()
+        ''' Train the SVM model '''
+        if not is_linearly_separable(X, y):
+            raise ValueError("Data is not linearly separable")
+        
+        n = X.shape[0]
+        K = np.dot(X, X.T)
+        y = np.where(y == 0, -1, 1)
+        astar = Optimizer.optimize(K, y)
+        
+        # One vs One classification
+        phi = np.zeros((n, n))
+        for i in range(10):
+            for j in range(i + 1, 10):
+                X_ij, y_ij = one_vs_one(X, y, i, j)
+                phi_ij = Optimizer.optimize(np.dot(X_ij, X_ij.T), y_ij)
+                phi += np.outer(y_ij, y_ij) * phi_ij
+        decision_value = np.sum([phi[i] * y_train[i] * np.dot(X_train[i], X) for i in range(n)]) + astar
+        return decision_value
+    return 
